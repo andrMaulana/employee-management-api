06cf8644a6077f7035a5408f9a5ca75f902d8295
AndrMaulana <andryymaulana2000@gmail.com>
2024-07-10 22:00:07 +0700

    Refactor(Department) : move interface repository and service
    - add scope method for hide soft delete

--- internal/domain/department/department.go
+++ internal/domain/department/department.go
@@ -1,29 +1,45 @@
 package department
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Department struct {
-	ID        uint       `json:"id" gorm:"primaryKey;column:Department_id"`
-	Name      string     `json:"name" gorm:"column:Department_name"`
-	CreatedAt time.Time  `json:"created_at" gorm:"column:Created_at"`
-	CreatedBy string     `json:"created_by" gorm:"column:Created_by"`
-	UpdatedAt time.Time  `json:"updated_at" gorm:"column:Updated_at"`
-	UpdatedBy string     `json:"updated_by" gorm:"column:Updated_by"`
-	DeletedAt *time.Time `json:"deleted_at,omitempty" gorm:"column:Deleted_at"`
+	ID        uint       `json:"id" gorm:"primaryKey;column:department_id"`
+	Name      string     `json:"name" gorm:"column:department_name"`
+	CreatedAt time.Time  `json:"created_at" gorm:"column:created_at"`
+	CreatedBy string     `json:"created_by" gorm:"column:created_by"`
+	UpdatedAt time.Time  `json:"updated_at" gorm:"column:updated_at"`
+	UpdatedBy string     `json:"updated_by" gorm:"column:updated_by"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty" gorm:"column:deleted_at"`
 }
 
-type Repository interface {
-	Create(department *Department) error
-	FindAll() ([]Department, error)
-	FindByID(id uint) (*Department, error)
-	Update(department *Department) error
-	Delete(id uint) error
+func (Department) TableName() string {
+	return "departments"
 }
 
-type Service interface {
-	Create(department *Department) error
-	GetAll() ([]Department, error)
-	GetByID(id uint) (*Department, error)
-	Update(department *Department) error
-	Delete(id uint) error
+func (Department) Scopes() map[string]func(*gorm.DB) *gorm.DB {
+	return map[string]func(*gorm.DB) *gorm.DB{
+		"active": func(db *gorm.DB) *gorm.DB {
+			return db.Where("Deleted_at IS NULL")
+		},
+	}
 }
+
+// type Repository interface {
+// 	Create(department *Department) error
+// 	FindAll() ([]Department, error)
+// 	FindByID(id uint) (*Department, error)
+// 	Update(department *Department) error
+// 	Delete(id uint) error
+// }
+
+// type Service interface {
+// 	Create(department *Department) error
+// 	GetAll() ([]Department, error)
+// 	GetByID(id uint) (*Department, error)
+// 	Update(department *Department) error
+// 	Delete(id uint) error
+// }
