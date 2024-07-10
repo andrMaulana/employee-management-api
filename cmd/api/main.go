2c2de7f93122a98241defda63693603792837ea7
AndrMaulana <andryymaulana2000@gmail.com>
2024-07-11 00:40:08 +0700

    Refactor(main) : change port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -34,5 +34,5 @@ func main() {
 	app.Put("/departments/batch", departmentHandler.BatchUpdate)
 	app.Delete("/departments/batch", departmentHandler.BatchDelete)
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":8080"))
 }
