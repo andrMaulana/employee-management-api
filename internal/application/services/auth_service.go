82adbd7783abfb704123be6a4d636d8821c5c635
AndrMaulana <andryymaulana2000@gmail.com>
2024-07-11 16:10:18 +0700

    Refactor(Auth) : add loging for auth login

--- internal/application/services/auth_service.go
+++ internal/application/services/auth_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"log"
 
 	"github.com/andrMaulana/employee-management-api/internal/domain/employee"
 	"github.com/andrMaulana/employee-management-api/internal/infrastructure/auth"
@@ -22,19 +23,45 @@ func NewAuthService(employeeRepo employee.Repository) AuthService {
 }
 
 func (s *authService) Login(ctx context.Context, employeeCode, password string) (string, error) {
+	// emp, err := s.employeeRepo.GetByEmployeeCode(ctx, employeeCode)
+	// if err != nil {
+	// 	return "", errors.ErrInvalidCredentials
+	// }
+
+	// if err := bcrypt.CompareHashAndPassword([]byte(emp.Password), []byte(password)); err != nil {
+	// 	return "", errors.ErrInvalidCredentials
+	// }
+
+	// token, err := auth.GenerateToken(emp.EmployeeID, emp.EmployeeName)
+	// if err != nil {
+	// 	return "", errors.ErrInternalServer
+	// }
+
+	// return token, nil
+
+	log.Printf("Attempting login for employee code: %s", employeeCode)
+
 	emp, err := s.employeeRepo.GetByEmployeeCode(ctx, employeeCode)
 	if err != nil {
-		return "", errors.ErrInvalidCredentials
+		if err == errors.ErrNotFound {
+			log.Printf("Login failed: Employee with code %s not found", employeeCode)
+			return "", errors.ErrInvalidCredentials
+		}
+		log.Printf("Login failed: Error fetching employee data: %v", err)
+		return "", errors.ErrInternalServer
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(emp.Password), []byte(password)); err != nil {
+		log.Printf("Login failed: Invalid password for employee code %s", employeeCode)
 		return "", errors.ErrInvalidCredentials
 	}
 
 	token, err := auth.GenerateToken(emp.EmployeeID, emp.EmployeeName)
 	if err != nil {
+		log.Printf("Login failed: Error generating token: %v", err)
 		return "", errors.ErrInternalServer
 	}
 
+	log.Printf("Login successful for employee code: %s", employeeCode)
 	return token, nil
 }
