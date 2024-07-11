6376a53b08960f5246d47641b7eaacaa068e1f4a
AndrMaulana <andryymaulana2000@gmail.com>
2024-07-11 16:09:48 +0700

    Refactor(Employee) : add logging for get code employee

--- internal/domain/employee/employee_repository.go
+++ internal/domain/employee/employee_repository.go
@@ -2,7 +2,9 @@ package employee
 
 import (
 	"context"
+	"log"
 
+	"github.com/andrMaulana/employee-management-api/pkg/errors"
 	"gorm.io/gorm"
 )
 
@@ -51,10 +53,27 @@ func (r *repository) Delete(ctx context.Context, id uint) error {
 }
 
 func (r *repository) GetByEmployeeCode(ctx context.Context, code string) (*Employee, error) {
+	// var employee Employee
+	// err := r.db.WithContext(ctx).Where("employee_code = ?", code).First(&employee).Error
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return &employee, nil
+
 	var employee Employee
+
+	log.Printf("Fetching employee with code: %s", code)
+
 	err := r.db.WithContext(ctx).Where("employee_code = ?", code).First(&employee).Error
 	if err != nil {
-		return nil, err
+		if err == gorm.ErrRecordNotFound {
+			log.Printf("Employee with code %s not found", code)
+			return nil, errors.ErrNotFound
+		}
+		log.Printf("Error fetching employee with code %s: %v", code, err)
+		return nil, errors.ErrInternalServer
 	}
+
+	log.Printf("Successfully fetched employee with code: %s", code)
 	return &employee, nil
 }
